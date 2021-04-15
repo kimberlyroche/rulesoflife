d72367055a5cb29025336596a48484e80a1d252c
Kimberly Roche <ker48@x2-06-1.genome.duke.edu>
2021-04-15 13:37:26 -0400

    HARDAC changes

--- fit_model.R
+++ fit_model.R
@@ -1,3 +1,8 @@
+if(R.version$major == 4 & Sys.info()[[1]] != "Windows") {
+  cat("Updating lib paths...\n")
+  .libPaths(c("/gpfs/fs1/data/mukherjeelab/roche/Rlibs", .libPaths()[2]))
+}
+
 library(optparse)
 library(rulesoflife)
 
@@ -5,7 +10,9 @@ option_list = list(
   make_option(c("--sname"), type = "character", default = NULL,
               help = "short name of host series to fit", metavar = "character"),
   make_option(c("--method"), type = "character", default = "GP",
-              help = "model: GP or DLM", metavar = "character")
+              help = "model: GP or DLM", metavar = "character"),
+  make_option(c("--MAP"), type = "logical", default = FALSE,
+              help = "MAP estimation flag", metavar = "logical")
 );
 
 opt_parser = OptionParser(option_list = option_list);
@@ -18,15 +25,16 @@ if(is.null(opt$sname)) {
 # Load
 data <- load_data(tax_level = "family")
 
+point_est <- opt$MAP
 if(opt$method == "DLM") {
   fit <- fit_DLM(sname = opt$sname,
                  counts = data$counts,
                  metadata = data$metadata,
-                 point_est = FALSE,
+                 point_est = point_est,
                  smoothed = TRUE)
 } else if(opt$method == "GP") {
   fit <- fit_GP(sname = opt$sname,
                 counts = data$counts,
                 metadata = data$metadata,
-                point_est = FALSE)
+                point_est = point_est)
 }
