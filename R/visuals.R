2d5333e4605f5a7b7f40b6977bab3e8c27407db2
kimberlyroche <kimberly.e.roche@gmail.com>
2021-04-15 13:40:32 -0400

    Added visualization of pairs of taxa across hosts

--- R/visuals.R
+++ R/visuals.R
@@ -178,3 +178,252 @@ plot_trajectory <- function(fit, coord, coord_label = NULL,
   }
 }
 
+#' Summarize the pairwise correlations (optionally via proportionality) from all
+#' fitted models of a given type. Uses MAP estimates if available, otherwise,
+#' summarizes full posterior model fits to give a "rug" matrix where rows are
+#' hosts and columns are unique pairwise interactions between taxa.
+#'
+#' @param method model fit method, "GP" or "DLM"
+#' @param use_proportionality flag indicating whether to compute proportionality
+#' between features; if FALSE, CLR correlations are computed
+#' @return named list of "rug" and associated labels
+#' @import fido
+#' @export
+summarize_Sigmas <- function(method = "GP", use_proportionality = FALSE) {
+  if(!(method %in% c("GP", "DLM"))) {
+    stop("Method not recognized!")
+  }
+  # Get all fitted model objects
+  output_dir <- check_dir(c("output", paste0(method, "_fits"), "MAP"))
+  # Collapse to object with rownames, 2 sets column names
+  file_list <- list.files(path = output_dir, pattern = "*.rds")
+  if(length(file_list) == 0) {
+    # Pull D
+    output_dir <- check_dir(c("output", paste0(method, "_fits")))
+    file_list <- list.files(path = output_dir, pattern = "*.rds")
+    if(length(file_list) == 0) {
+      stop("No fitted models found!")
+    }
+    fit <- readRDS(file.path(output_dir, file_list[1]))
+    D <- fit$D
+    pairs <- combn(1:(fit$D), m = 2)
+    pair1 <- pairs[1,]
+    pair2 <- pairs[2,]
+    rug <- matrix(NA, length(file_list), D*(D - 1)/2)
+    hosts <- character(length(file_list))
+    # Summarize full posteriors (via mean)
+    for(f in 1:length(file_list)) {
+      file <- file_list[f]
+      fit <- readRDS(file.path(output_dir, file))
+      # Convert to CLR
+      if(fit$coord_system != "clr") {
+        fit <- to_clr(fit)
+      }
+      if(use_proportionality) {
+        Sigma <- apply(fit$Sigma, c(1,2), mean)
+        for(m in 1:ncol(pairs)) {
+          j <- pair1[m]
+          k <- pair2[m]
+          var_j <- Sigma[j,j] # diagonal element
+          var_k <- Sigma[k,k] # diagonal element
+          var_j_minus_k <- var_j + var_k - 2*Sigma[j,k]
+          rho_jk <- 1 - var_j_minus_k / (var_j + var_k)
+          rug[f,m] <- rho_jk
+        }
+      } else {
+        Sigma_correlation <- fit$Sigma
+        for(i in 1:fit$iter) {
+          Sigma_correlation[,,i] <- cov2cor(Sigma_correlation[,,i])
+        }
+        Sigma_summary <- apply(Sigma_correlation, c(1,2), mean)
+        vector_Sigma <- Sigma_summary[upper.tri(Sigma_summary, diag = FALSE)]
+        rug[f,] <- vector_Sigma
+      }
+      hosts[f] <- fit$sname
+    }
+  } else {
+    # Use MAP
+    # Pull D
+    fit <- readRDS(file.path(output_dir, file_list[1]))
+    D <- fit$D
+    pairs <- combn(1:(fit$D), m = 2)
+    pair1 <- pairs[1,]
+    pair2 <- pairs[2,]
+    rug <- matrix(NA, length(file_list), D*(D - 1)/2)
+    hosts <- character(length(file_list))
+    # Summarize full posteriors (via mean)
+    for(f in 1:length(file_list)) {
+      file <- file_list[f]
+      fit <- readRDS(file.path(output_dir, file))
+      # Convert to CLR
+      if(fit$coord_system != "clr") {
+        fit <- to_clr(fit)
+      }
+      if(use_proportionality) {
+        Sigma <- fit$Sigma[,,1]
+        for(m in 1:ncol(pairs)) {
+          j <- pair1[m]
+          k <- pair2[m]
+          var_j <- Sigma[j,j] # diagonal element
+          var_k <- Sigma[k,k] # diagonal element
+          var_j_minus_k <- var_j + var_k - 2*Sigma[j,k]
+          rho_jk <- 1 - var_j_minus_k / (var_j + var_k)
+          rug[f,m] <- rho_jk
+        }
+      } else {
+        Sigma_correlation <- cov2cor(fit$Sigma[,,1])
+        vector_Sigma <- Sigma_correlation[upper.tri(Sigma_correlation,
+                                                    diag = FALSE)]
+        rug[f,] <- vector_Sigma
+      }
+      hosts[f] <- fit$sname
+    }
+  }
+  return(list(hosts = hosts, rug = rug, tax_idx1 = pair1, tax_idx2 = pair2))
+}
+
+#' Utility function for pulling collection date-aligned trajectories from a
+#' given host's prediction set.
+#'
+#' @import fido
+get_trajectory_df <- function(dates, coord, common_baseline, predictions,
+                              center = NULL) {
+  # Get days from common baseline
+  days <- round(unname(sapply(dates, function(x) {
+      difftime(x, common_baseline, units = "days")
+    }))) + 1
+  # Recalculate the "days" index for this host given the new baseline
+  offset <- days[1] - 1
+
+  days <- predictions$span + offset
+  eta_df <- gather_array(predictions$Eta, val, coord, sample, iteration)
+  map <- data.frame(sample = 1:max(eta_df$sample), day = days)
+  eta_df <- left_join(eta_df, map, by = "sample")
+  eta_df <- eta_df %>%
+    select(coord, day, iteration, val) %>%
+    group_by(coord, day) %>%
+    summarize(p2.5 = quantile(val, probs = c(0.025)),
+              p25 = quantile(val, probs = c(0.25)),
+              mean = mean(val),
+              p75 = quantile(val, probs = c(0.75)),
+              p97.5 = quantile(val, probs = c(0.975)), .groups = "keep")
+  plot_df <- eta_df[eta_df$coord == coord,]
+  if(!is.null(center)) {
+    new_center <- mean(plot_df$mean) + center
+    plot_df <- plot_df %>%
+      mutate(p2.5 = p2.5 - new_center,
+             p25 = p25 - new_center,
+             p75 = p75 - new_center,
+             p97.5 = p97.5 - new_center,
+             mean = mean - new_center)
+  }
+  return(plot_df)
+}
+
+#' Utility function for pulling collection date-aligned trajectories for a pair
+#' of taxa from a given host's prediction set.
+get_paired_trajectories <- function(dates, coord1, coord2, host, common_baseline,
+                                    predictions, center = NULL) {
+  pair_df_tax1 <- get_trajectory_df(dates,
+                                    coord1,
+                                    common_baseline,
+                                    predictions,
+                                    center = center)
+  pair_df_tax1 <- cbind(host = host, pair_df_tax1)
+  pair_df_tax2 <- get_trajectory_df(dates,
+                                    coord2,
+                                    common_baseline,
+                                    predictions,
+                                    center = center)
+  pair_df_tax2 <- cbind(host = host, pair_df_tax2)
+  pair_df <- rbind(pair_df_tax1, pair_df_tax2)
+  return(pair_df)
+}
+
+#' Utility function for pulling collection date-aligned trajectories for a pair
+#' of taxa from a set of hosts.
+get_predictions_host_list <- function(ref_hosts, metadata) {
+  prediction_list <- list()
+  date_list <- list()
+  for(host in ref_hosts) {
+    fit <- readRDS(check_dir(file.path("output", "GP_fits", paste0(host, ".rds"))))
+    predictions <- predict_trajectory(fit)
+    dates <- metadata %>%
+      filter(sname == fit$sname) %>%
+      pull(collection_date)
+    prediction_list[[host]] <- predictions
+    date_list[[host]] <- dates
+  }
+  # Find common baseline date
+  common_baseline <- min(unname(unlist(date_list)))
+  return(list(common_baseline = common_baseline,
+              predictions = prediction_list,
+              dates = date_list))
+}
+
+#' Render date-aligned trajectories for a pair of taxa and a given set of hosts.
+#' This allows us to eyeball whether taxa strongly correlated within hosts show
+#' similar dynamics across hosts.
+#'
+#' @param host_list vector of host short names
+#' @param tax_idx1 index of first taxon
+#' @param tax_idx2 index of second taxon
+#' @param metadata metadata data.frame (with sname and collection_date column)
+#' @param save_file flag indicating whether or not to save rendered plot to file
+#' @return NULL
+#' @export
+plot_aligned_trajectories <- function(host_list, tax_idx1, tax_idx2, metadata,
+                                      save_file = FALSE) {
+  # Pull models/predictions
+  pred_obj <- get_predictions_host_list(host_list, metadata)
+  host_centers <- seq(from = 0, by = 5, length.out = length(host_list))
+  names(host_centers) <- sort(host_list, decreasing = TRUE)
+  pair_df <- NULL
+  for(host in host_list) {
+    pair_df_host <- get_paired_trajectories(pred_obj$dates[[host]],
+                                            tax_idx1,
+                                            tax_idx2,
+                                            host,
+                                            pred_obj$common_baseline,
+                                            pred_obj$predictions[[host]],
+                                            center = host_centers[[host]])
+    if(is.null(pair_df)) {
+      pair_df <- pair_df_host
+    } else {
+      pair_df <- rbind(pair_df, pair_df_host)
+    }
+  }
+
+  # The pair_df data.frame contains the interpolated trajectories (50% CI and 95%
+  # CI) for a pair of taxa in two hosts.
+  head(pair_df)
+
+  p <- ggplot()
+  for(ref_host in host_list) {
+    p <- p +
+      geom_ribbon(data = pair_df[pair_df$host == ref_host & pair_df$coord == tax_idx1, ],
+                  aes(x = day, ymin = p25, ymax = p75),
+                  fill = "red",
+                  alpha = 0.33) +
+      geom_ribbon(data = pair_df[pair_df$host == ref_host & pair_df$coord == tax_idx2, ],
+                  aes(x = day, ymin = p25, ymax = p75),
+                  fill = "blue",
+                  alpha = 0.33)
+  }
+  p <- p + scale_y_continuous(breaks = -unname(host_centers),
+                              labels = sort(host_list, decreasing = TRUE))
+
+  if(save_file) {
+    filename <- paste0("aligned_series_coords_", tax_idx1, "-", tax_idx2, "_",
+                       paste0(host_list, collapse = "-"), ".png")
+    output_dir <- check_dir(c("output", "images"))
+    ggsave(file.path(output_dir, filename),
+           p,
+           units = "in",
+           height = length(host_list),
+           width = 10)
+  } else {
+    show(p)
+  }
+}
+
--- walkthrough.R
+++ walkthrough.R
@@ -1,44 +1,26 @@
 rm(list = ls())
 
+library(tidyverse)
 library(rulesoflife)
 library(fido)
 
 # Load
 data <- load_data(tax_level = "family")
 
-# Chop down VAP's series for DLM test
-VAP_idx <- which(data$metadata$sname == "VAP")
-dates <- data$metadata$collection_date[VAP_idx]
-# unname(sapply(dates, function(x) difftime(x, min(dates), units = "days")))
-VAP_idx <- VAP_idx[4:15]
-
-data$counts <- data$counts[,VAP_idx]
-data$metadata <- data$metadata[VAP_idx,]
-
-counts <- data$counts
-metadata <- data$metadata
-sname <- "VAP"
-point_est <- FALSE
-smoothed <- FALSE
-
 # Fit sample model
 # fit <- fit_GP(sname = "VAP", counts = data$counts, metadata = data$metadata, point_est = FALSE)
-fit <- fit_DLM(sname = "VAP", counts = data$counts, metadata = data$metadata, point_est = FALSE, smoothed = TRUE)
+# fit <- fit_DLM(sname = "VAP", counts = data$counts, metadata = data$metadata, point_est = FALSE, smoothed = TRUE)
 
-str(fit)
+# plot_trajectory(fit,
+#                 coord = coord,
+#                 coord_label = get_tax_label(data$taxonomy, 1, "CLR"),
+#                 show_observations = TRUE,
+#                 save_file = FALSE)
 
-coord <- 1
+plot_aligned_trajectories(host_list = c("ACA", "ALE", "CAI", "COB", "COO", "DAG"),
+                          tax_idx1 = 1,
+                          tax_idx2 = 2,
+                          metadata = data$metadata,
+                          save_file = TRUE)
 
-fit <- readRDS("output/GP_fits/VAP.rds")
-plot_trajectory(fit,
-                coord = coord,
-                coord_label = get_tax_label(data$taxonomy, 1, "CLR"),
-                show_observations = TRUE,
-                save_file = FALSE)
 
-fit <- readRDS("output/DLM_fits/VAP.rds")
-plot_trajectory(fit,
-                coord = 1,
-                coord_label = get_tax_label(data$taxonomy, 1, "ALR"),
-                show_observations = TRUE,
-                save_file = FALSE)
