8cfe3c4d72b0e06beb084df2c1563070192ccdc1
Kimberly Roche <ker48@x2-02-1.genome.duke.edu>
2021-07-19 15:27:30 -0400

    Synching R^2 manually

--- scratch_cont_ANOVA.R
+++ scratch_cont_ANOVA.R
@@ -1,17 +1,33 @@
+source("path_fix.R")
+
 library(rulesoflife)
 library(fido)
+library(driver)
 library(shapes)
 library(tidyverse)
+library(vegan)
 
 output_dir <- "asv_days90_diet25_scale1"
-n_iter <- 10
+testing <- TRUE # subset hosts and posterior samples
+
+# ------------------------------------------------------------------------------
+#   Load baseline and dynamics estimates and calculate sets of distances between
+#   them
+#
+#   I'm using the ALR for now but we probably want to consider which data
+#   representation is best.
+# ------------------------------------------------------------------------------
 
 # Load a few posterior samples for a few hosts
 data <- load_data(tax_level = "ASV")
 md <- data$metadata
 hosts <- sort(unique(md$sname))
+if(testing) {
+  hosts <- sample(hosts, size = 20)
+}
 
-Sigmas <- NULL
+Sigmas <- list()
+n_iter <- NULL
 for(host in hosts) {
   cat(paste0("Parsing host ", host, "...\n"))
   fit <- readRDS(file.path("output",
@@ -19,69 +35,135 @@ for(host in hosts) {
                            output_dir,
                            "full_posterior",
                            paste0(host, ".rds")))
-  fit.clr <- to_clr(fit)
-  Sigmas[[host]] <- fit.clr$Sigma[,,sample(1:fit$iter, size = n_iter)]
+  # CLR
+  fit <- to_clr(fit)
+  if(is.null(n_iter)) {
+    if(testing) {
+      n_iter <- 20
+    } else {
+      n_iter <- dim(fit$Sigma)[3]
+    }
+  }
+  Sigmas[[host]] <- fit$Sigma[,,1:n_iter]
 }
 D <- dim(Sigmas[[1]])[1]
 
-# Load Frechet mean (need to re-run this on proper MAP estimates)
-# Nvm; no need here
-# Frechet_mean <- readRDS(file.path("output", "Frechet_mean.rds"))
+# Calculate samples of the dynamics distance
+host_combos <- combn(length(hosts), m = 2)
+dynamics_distances <- matrix(NA, ncol(host_combos), n_iter)
+for(i in 1:n_iter) {
+  cat(paste0("Calculating distances for sample ", i, "\n"))
+  dynamics_distances[,i] <- apply(host_combos, 2, function(pair) {
+    distcov(Sigmas[[pair[1]]][,,i] + diag(D)*1e-06,
+            Sigmas[[pair[2]]][,,i] + diag(D)*1e-06,
+            method = "Riemannian")
+  })
+}
+
+# ------------------------------------------------------------------------------
+#   Load and test PEDIGREE
+# ------------------------------------------------------------------------------
 
 # Load pedigree
 pedigree <- readRDS(file.path("input", "pedigree_56hosts.RDS"))
-# Reorder
-pedigree <- pedigree[order(rownames(pedigree)),order(colnames(pedigree))]
+mapping <- data.frame(sname = names(Sigmas)) %>%
+  left_join(data.frame(sname = rownames(pedigree), order = 1:nrow(pedigree)), by = "sname")
+pedigree <- pedigree[mapping$order,mapping$order] # subset and order as Sigmas
+ped_dist <- 1 - pedigree
 
-# Calculate dynamics distance
-s <- 1 # sample i
-limit <- 1000
-# limit <- length(host_combos)
-host_combos <- combn(length(hosts), m = 2)
-dynamics_distances <- numeric(limit)
-pedigree_vec <- numeric(limit)
-for(i in 1:limit) {
-  if(i %% 100 == 0) {
-    cat(paste0("Iteration ", i, "\n"))
-  }
-  host1 <- hosts[host_combos[1,i]]
-  host2 <- hosts[host_combos[2,i]]
-  dynamics_distances[i] <- distcov(Sigmas[[host1]][,,s] + diag(D)*1e-06,
-                                   Sigmas[[host2]][,,s] + diag(D)*1e-06,
-                                   method = "Riemannian")
-  pedigree_vec[i] <- pedigree[host_combos[1,i],host_combos[2,i]]
+x <- ped_dist[lower.tri(ped_dist, diag = FALSE)]
+x <- scale(x)
+
+sampled_corr_ped <- c()
+for(i in 1:n_iter) {
+  y <- dynamics_distances[,i]
+  y <- scale(y)
+  sampled_corr_ped <- c(sampled_corr_ped, cor(x, y)**2)
 }
-dynamics_distances <- scale(dynamics_distances)
-pedigree_vec <- scale(pedigree_vec)
 
-cor(dynamics_distances, pedigree_vec)**2
-
-ggplot(data.frame(x = dynamics_distances, y = pedigree_vec),
+# Plot a single posterior sample
+pl <- ggplot(data.frame(x = x, y = y),
        aes(x = x, y = y)) +
-  geom_point(size = 3, shape = 21, fill = "#aaaaaa") +
+  geom_point(size = 3, shape = 21, fill = "#dddddd") +
+  # geom_smooth(method = "lm", color = "black") +
   theme_bw() +
-  labs(x = "dynamics distance",
-       y = "pedigree similarity")
+  labs(x = "pedigree dissimilarity (scaled)",
+       y = "dynamics distance (scaled)",
+       title = paste0("Dynamics x pedigree: R^2 = ", round(mean(sampled_corr_ped), 3), " +/- ", round(sd(sampled_corr_ped), 3)))
+show(pl)
+ggsave(file.path("output", "figures", "pedigree_R2.png"),
+       plot = pl,
+       dpi = 100,
+       units = "in",
+       height = 4,
+       width = 5)
 
 # ------------------------------------------------------------------------------
-#   Is a Mantel test even possible here?
+#   Mantel results from the same
 # ------------------------------------------------------------------------------
 
-library(vegan)
-
-# < 1 min. runtime
+# Tedious and bad code but... render (by re-calculating) these distances as a
+# matrix
 dynamics_distances <- matrix(0, length(hosts), length(hosts))
 for(i in 1:ncol(host_combos)) {
+  cat(paste0("Host combo ", i, " / ", ncol(host_combos), "\n"))
   h1 <- host_combos[1,i]
   h2 <- host_combos[2,i]
   dynamics_distances[h1, h2] <- distcov(Sigmas[[h1]][,,1] + diag(D)*1e-06,
-          Sigmas[[h2]][,,1] + diag(D)*1e-06,
-          method = "Riemannian")
+                                        Sigmas[[h2]][,,1] + diag(D)*1e-06,
+                                        method = "Riemannian")
   dynamics_distances[h2, h1] <- dynamics_distances[h1, h2]
 }
 
-pedigree_dissimilarities <- 1 - pedigree
+fit <- mantel(ped_dist, dynamics_distances)
+cat(paste0("Mantel p-value (dynamics x composition): ", round(fit$signif, 5), "\n"))
 
-mantel(pedigree_dissimilarities, dynamics_distances)
+# ------------------------------------------------------------------------------
+#   Load and test BASELINE COMPOSITION
+# ------------------------------------------------------------------------------
 
-# Next: Is using R^2 as a plug-in for variance explained reasonable?
+# Calculate baseline composition -- mean Eta for each host
+baselines <- matrix(NA, length(hosts), D)
+for(i in 1:length(hosts)) {
+  # Average of ALR samples for this host is their baseline
+  host_samples <- clr_array(data$counts[,which(md$sname == hosts[i])] + 0.5,
+                            parts = 1)
+  baselines[i,] <- apply(host_samples, 1, mean)
+}
+
+x <- c(dist(baselines))
+x <- scale(x)
+sampled_corr_base <- c()
+for(i in 1:n_iter) {
+  y <- dynamics_distances[,i]
+  y <- scale(y)
+  sampled_corr_base <- c(sampled_corr_base, cor(x, y)**2)
+}
+
+# Plot a single posterior sample
+pl <- ggplot(data.frame(x = x, y = y),
+             aes(x = x, y = y)) +
+  geom_point(size = 3, shape = 21, fill = "#dddddd") +
+  # geom_smooth(method = "lm", color = "black") +
+  theme_bw() +
+  labs(x = "baseline compositional distance (scaled)",
+       y = "dynamics distance (scaled)",
+       title = paste0("Dynamics x composition: R^2 = ",
+                      round(mean(sampled_corr_base), 3),
+                      " +/- ",
+                      round(sd(sampled_corr_base), 3)))
+show(pl)
+ggsave(file.path("output", "figures", "composition_R2.png"),
+       plot = pl,
+       dpi = 100,
+       units = "in",
+       height = 4,
+       width = 5)
+
+# ------------------------------------------------------------------------------
+#   Mantel results from the same
+# ------------------------------------------------------------------------------
+
+fit <- mantel(dist(baselines), dynamics_distances)
+cat(paste0("Mantel p-value (dynamics x composition): ", round(fit$signif, 5), "\n"))
+
