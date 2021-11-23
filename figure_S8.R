712f75ed8dca4d780b58e879aa383b12420fc22f
kimberlyroche <kimberly.e.roche@gmail.com>
2021-11-23 07:17:45 -0500

    Changed matrix Frechet mean calculation to frechet package

--- figure_S8.R
+++ figure_S8.R
@@ -1,14 +1,24 @@
 source("path_fix.R")
 
+library(tidyverse)
 library(rulesoflife)
-library(shapes)
+# library(shapes)
+library(frechet)
 library(matrixsampling)
+library(ggridges)
 
 # These are calculated by `analysis_compute_Frechets.R`
 # We ultimately need to roll that script's content into this one
-F1 <- readRDS(file.path("output", "Frechet_1.rds"))
-F2 <- readRDS(file.path("output", "Frechet_2.rds"))
-F3 <- readRDS(file.path("output", "Frechet_3.rds"))
+F1 <- readRDS(file.path("output", "Frechet_1_corr.rds"))
+F2 <- readRDS(file.path("output", "Frechet_2_corr.rds"))
+F3 <- readRDS(file.path("output", "Frechet_3_corr.rds"))
+
+# This function from the frechet package runs faster and gives a different (and
+# frankly more intuitive) answer than the shapes package estimates, where means
+# had a very very different scale than the samples they were computed from.
+F1$mean <- CovFMean(F1$Sigmas)$Mout[[1]]
+F2$mean <- CovFMean(F2$Sigmas)$Mout[[1]]
+F3$mean <- CovFMean(F3$Sigmas)$Mout[[1]]
 
 # Compute squared distances in each case as our variance analog
 D <- dim(F1$Sigmas)[1]
@@ -35,21 +45,23 @@ plot_df <- data.frame(x = c(D1,
                       label = c(rep("true", N),
                                 rep("permuted", N),
                                 rep("random", N)))
-plot_df$label <- factor(plot_df$label, levels = c("true", "permuted", "random"))
-levels(plot_df$label) <- c("Observed", "Permutated MAP", "Random dynamics")
+plot_df$label <- factor(plot_df$label, levels = c("random", "permuted", "true"))
+levels(plot_df$label) <- c("Random", "Permutated", "Observed")
+
 p <- ggplot(plot_df,
-            aes(x = x, fill = label)) +
-  geom_density(alpha = 0.5) +
+            aes(x = x, y = label, fill = label)) +
+  geom_density_ridges(alpha = 0.8)  +
   theme_bw() +
   labs(x = "distance from mean dynamics",
+       y = "",
        fill = "Data source") +
-  xlim(c(0, 1000))
+  theme(legend.position = "none")
 
 ggsave(file.path("output", "figures", "S8.svg"),
        dpi = 100,
        units = "in",
-       height = 4.5,
-       width = 7)
+       height = 4,
+       width = 6)
 
 v1 <- sum(D1) / (N-1)
 v2 <- sum(D2) / (N-1)
